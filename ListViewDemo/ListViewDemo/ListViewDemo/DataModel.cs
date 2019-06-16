e4822b10d9467d0730925dc4c649d0a2d9a7d595
Rahul Kumar <rk05315@gmail.com>
2019-06-16 10:16:14 +0530

    Added modal class. Listview item addition.

---
+++ ListViewDemo/ListViewDemo/ListViewDemo/DataModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ListViewDemo
+{
+    public class DataModel
+    {
+        public DataModel()
+        {
+        }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
--- ListViewDemo/ListViewDemo/ListViewDemo/MainPage.xaml.cs
+++ ListViewDemo/ListViewDemo/ListViewDemo/MainPage.xaml.cs
@@ -13,9 +13,39 @@ namespace ListViewDemo
     [DesignTimeVisible(true)]
     public partial class MainPage : ContentPage
     {
+        public List<DataModel> contactDetails;
         public MainPage()
         {
             InitializeComponent();
+            PopulateDetails();
+            listView.ItemsSource = contactDetails;
+        }
+
+        private void PopulateDetails()
+        {
+            if (contactDetails == null)
+            {
+                contactDetails = new List<DataModel>();
+                contactDetails.Add(new DataModel() { Name = "Anderson", PhoneNumber = "Martin" });
+                contactDetails.Add(new DataModel() { Name = "Adam", PhoneNumber = "Dev" });
+                contactDetails.Add(new DataModel() { Name = "Bob", PhoneNumber = "Harry"});
+                contactDetails.Add(new DataModel() { Name = "Brooke", PhoneNumber = "Clancy" });
+                contactDetails.Add(new DataModel() { Name = "Bella", PhoneNumber = "Jo" });
+                contactDetails.Add(new DataModel() { Name = "Charlie", PhoneNumber = "James" });
+                contactDetails.Add(new DataModel() { Name = "Carla", PhoneNumber = "Buckner" });
+                contactDetails.Add(new DataModel() { Name = "Dev", PhoneNumber = "Anand" });
+                contactDetails.Add(new DataModel() { Name = "Fried", PhoneNumber = "Hardy" });
+                contactDetails.Add(new DataModel() { Name = "Harry", PhoneNumber = "Jacob" });
+                contactDetails.Add(new DataModel() { Name = "Hannah", PhoneNumber = "Icaza" });
+                contactDetails.Add(new DataModel() { Name = "Michael", PhoneNumber = "Nikon" });
+                contactDetails.Add(new DataModel() { Name = "Maria", PhoneNumber = "Shara"});
+
+            }
+        }
+
+        void Handle_ItemSelected(object sender, Xamarin.Forms.SelectedItemChangedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
